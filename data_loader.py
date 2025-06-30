082ec8c495c3e7663e38d437533aeec5f02463cd
preeti khulbe <100253145+preetikhullbe@users.noreply.github.com>
2025-06-30 17:11:14 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -70,7 +70,7 @@ def save_live_data_to_parquet_chunks():
         max_retries=3,
         retry_on_timeout=True
     )
-    df = fetch_last_3_months_data(es)
+    df = fetch_last_months_data(es)
 
     if df.empty:
         print("No data to save.")

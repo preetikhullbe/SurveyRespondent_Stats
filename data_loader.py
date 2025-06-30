5c7eec05fbec991eb782b4cae75cf73b790142fd
preeti khulbe <100253145+preetikhullbe@users.noreply.github.com>
2025-06-30 18:00:13 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -17,7 +17,7 @@ if not all([cloud_id, username, password]):
 
 def fetch_last_3_months_data(es):
     end_date = datetime.now()
-    start_date = end_date - timedelta(days=30)
+    start_date = end_date - timedelta(days=20)
 
     start_str = start_date.strftime("%Y-%m-%dT%H:%M:%S")
     end_str = end_date.strftime("%Y-%m-%dT%H:%M:%S")

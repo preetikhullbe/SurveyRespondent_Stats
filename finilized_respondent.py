eae48b191a1d80f87d0e331a66457716d0b3b550
preeti khulbe <100253145+preetikhullbe@users.noreply.github.com>
2025-06-30 15:39:56 +0530

    Update finilized_respondent.py

--- finilized_respondent.py
+++ finilized_respondent.py
@@ -24,7 +24,7 @@ def process_data(df, output_file, start_date=None, end_date=None, client_filter=
       start_date = pd.to_datetime(start_date)
       filtered_df = filtered_df[filtered_df['survey_enddate'] >= start_date]
 
-   if end_date:
+    if end_date:
       end_date = pd.to_datetime(end_date)
       filtered_df = filtered_df[filtered_df['survey_enddate'] <= end_date]
 
